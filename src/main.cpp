afd76ce20dc5595fef870e1126059ced20d9b257
Zamurovka <victor17razumov@gmail.com>
2024-03-05 21:04:11 +0300

    New text

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,21 @@
-#include <iostream>
-
-int main(int, char**){
-    std::cout << "Hello, from TuweApp!";
+#include <ftxui/dom/elements.hpp>  // for text, operator|, bold, Fit, hbox, Element
+#include <ftxui/screen/screen.hpp>  // for Full, Screen
+#include <memory>                   // for allocator
+ 
+#include "ftxui/dom/node.hpp"      // for Render
+#include "ftxui/screen/color.hpp"  // for ftxui
+ 
+int main() {
+  using namespace ftxui;
+  auto document =  //
+      hbox({
+          text("This text is "),
+          text("bold") | bold,
+          text(". Do you like it?"),
+      });
+  auto screen = Screen::Create(Dimension::Full(), Dimension::Fit(document));
+  Render(screen, document);
+  screen.Print();
+ 
+  return 0;
 }
