f5878c41748d8acceb116749b6e0665d38b6b373
Zamurovka <victor17razumov@gmail.com>
2024-05-25 17:37:58 +0300

    Minor interface improvements!

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,8 @@
 #include <string>  // for string 
 #include <stdlib.h>
 #include "ftxui/component/captured_mouse.hpp"  // for ftxui
+#include <ftxui/screen/screen.hpp>  // for Pixel
+
 #include "ftxui/component/component.hpp"       // for Button, Renderer, Vertical
 #include "ftxui/component/component_options.hpp"   // for ButtonOption
 #include "ftxui/component/screen_interactive.hpp"  // for ScreenInteractive
@@ -15,7 +17,7 @@ using namespace ftxui;
 class MainWindow{
   public:
     //Data
-    string usertoa = "fuck";
+    string usertoa = "null";
   void dataReturn(){
     cout << usertoa << endl;
   }
@@ -29,6 +31,7 @@ class MainWindow{
   
 
   int humidity = 80;
+  string humid =to_string(humidity) + "%";
 
   //colours for text
   int colorH = 1;
@@ -37,13 +40,13 @@ class MainWindow{
 
   //SIZE OF 
   const int sizex = 20;
-  const int sizey = 3;
+  const int sizey = 10;
 
   int counter = 0;
 
   //input components
   Component input_colortest = Input(&help, "Color");
-  Component input_user = Input(&userText, "ENTER: ");
+  Component input_user = Input(&userText, "ENTER!: ");
   Component user_to_answer = Input(&usertoa, "Enter your request: ");
   //Component answer_area = 
   
@@ -59,6 +62,7 @@ class MainWindow{
   //city name color changing func
   auto wtf = [&]{
     int temp = stoi(help);
+    help += "	°";
     if (temp<=0){
       colorH = 120 + 0.8 * abs(temp);
     }
@@ -71,10 +75,28 @@ class MainWindow{
     input_user,
   });
 
+
+
+
+  // auto renderer_line_braille = Renderer([&] {
+  //   auto c = Canvas(100, 100);
+  //   c.DrawText(0, 0, "Several lines (braille)");
+  //   c.DrawPointLine(80, 10, 80, 40, Color::Blue);
+  //   return canvas(std::move(c));
+  // });
+
+
+  auto canv = Canvas(30, 30);
+  //canv.DrawText(0, 5, "Погода:");
+
+  canv.DrawBlockLine(6,5, 29, 25, Color::LightCoral);
+   
+
+
   auto style = ButtonOption::Animated(Color::Default, Color::DarkSlateGray1,
                                       Color::GrayDark, Color::White);
 
-  Component humiditySlider =   Slider( "Влажность: " , &humidity , 0 , 100 , 1 );
+  Component humiditySlider =   Slider( "" , &humidity , 0 , 100 , 1 );
   Component buttonuser = Button("Enter " , wtf, style);
 
   auto Container1 = Container::Vertical({
@@ -82,7 +104,45 @@ class MainWindow{
     input_colortest,
     user_to_answer,
     input_add,
+    
   });
+  auto ContainerFuture = Container::Horizontal({
+
+  });
+auto future_renderer = Renderer(ContainerFuture, [&] {
+    return vbox({
+      hbox({
+        filler(),
+        hbox({
+        filler(),
+        text("sus:"),
+        filler(),
+        })|border,
+        filler(),
+        hbox({
+          filler(),
+        text("sus:"),
+        filler(),
+        })|border,
+        filler(),
+        hbox({
+          filler(),
+        text("sus:"),
+        filler(),
+        })|border,
+        filler(),
+        hbox({
+          filler(),
+        text("sus:"),
+        filler(),
+        })|border,
+        filler(),
+      }),
+      filler(),
+    });
+});
+
+
 
   auto input_renderer = Renderer(InputBox, [&] {
     return vbox({
@@ -101,51 +161,141 @@ class MainWindow{
     });
   });
 
+
+
+    //виджеты
+  auto box_rendener = Renderer(Container1, [&] {
+  return vbox({
+
+  
+  //виджеты темп и влаж 
+  hbox({
+  
+
+  vbox({
+    
+filler(),
+    hbox({filler(),text("Температура"),filler(),}),
+
+
+filler(),
+  hbox({
+  filler(),
+    text(help),
+  filler(),
+  }),
+    
+    //humiditySlider->Render() ,
+    //filler(),
+    
+  })|border|size(WIDTH, EQUAL, sizex)|size(HEIGHT, EQUAL, sizey),
+  
+  filler(),
+  //виджет
+  vbox({
+    filler(),
+  hbox({
+    filler(),
+    text("Влажность "),
+    filler(),
+    }),
+    hbox({
+    filler(),
+    text(humid),
+    filler(),
+    }),
+  filler(),
+    filler(),
+  humiditySlider->Render() ,
+
+    filler(),
+    
+  //canv->Render(),
+  })|border|size(WIDTH, EQUAL, sizex)|size(HEIGHT, EQUAL, sizey),
+  
+  }),
+  filler(),
+
+  hbox({        //weather
+  filler(),
+  vbox({
+  filler(),
+    
+    hbox({filler(),text("Погода"),filler(),}),
+    filler(),
+    
+    
+  canvas(&canv),
+  filler(),
+
+  filler(),})|border|size(WIDTH, EQUAL, sizex)|size(HEIGHT, EQUAL, sizey),
+
+
+  filler(),
+  vbox({        //w
+  filler(),
+  hbox({
+    filler(),
+    text("Ветер" ) ,
+    filler(),
+  }),
+  filler(),
+  filler(),
+  hbox({
+
+    filler(),
+
+    text("←") ,
+    filler(),
+  }),
+  filler(),
+  })|border|size(WIDTH, EQUAL, sizex)|size(HEIGHT, EQUAL, sizey),
+  
+  //canvas(&canv) | border,
+  //filler(),
+    
+    
+    
+    
+}),
+   });
+});
+
   auto next_rendener = Renderer(Container1, [&] {
     return vbox({
 
     hbox({
-      filler(),
-          color(Color::NavajoWhite1,text("TUWE")),
+      
+          color(Color::RosyBrown,text("TUWE — ")),
+          color(Color::SkyBlue2 , text("Their Unbelievable Wisdom is Extraordinary")),
       filler(),
     }),
 
-    hbox({
-      filler(),
-            color(Color::SkyBlue2 , text("Their Unbelievable Wisdom is Extraordinary")),
-      filler(),
+    // hbox({
+    //   filler(),
+    //         color(Color::SkyBlue2 , text("Their Unbelievable Wisdom is Extraordinary")),
+    //   filler(),
       
-    }),
+    // }),
 
       separatorHSelector(0,1000,Color::White,Color::White),
 
     hbox({
-        filler(),
-        color(Color::HSV(colorH, colorS, colorV ),text("NIZHNY NOVGOROD"))|border,
+        
+        color(Color::HSV(colorH, colorS, colorV ),text("NIZHNY NOVGOROD")),
         filler(),  
       }),
-
+    filler(), 
       separator(),
-
-      hbox({
-      vbox({
-        filler(),
-        text("Температура: " + help),
-        filler(),
-        //humiditySlider->Render() ,
-        //filler(),
-        
-      })|border|size(WIDTH, EQUAL, sizex)|size(HEIGHT, EQUAL, sizey),
-
-      vbox({
-      filler(),
-        
-        humiditySlider->Render() ,
-        filler(),
-      })|border|size(WIDTH, EQUAL, sizex)|size(HEIGHT, EQUAL, sizey),
-
-      }),
-
+      future_renderer->Render(),
+        //коробка для виджетов
+        hbox({
+          filler(), 
+        box_rendener->Render(),
+        filler(), 
+        }),
+      
+      // box with color input
     hbox({
       filler(),
       vbox({
@@ -157,16 +307,28 @@ class MainWindow{
       }),                
       filler(),
     }),
+filler(),
+  separator(),
+filler(),
 
-  filler(),
-
-    hbox({
+    vbox({
       filler(),
-      text("" + usertoa),
-      filler(),
+      hbox({filler(),
       input_add->Render(),
+      
       filler(),
+      }),
+      filler(),
+      hbox({
+      
+      filler(),
+      text(usertoa),
+      filler(),
+      }),
+    filler(),
     }),
+    filler(),
+    text(""),
   }) |
   borderStyled(DOUBLE, Color::White);
   });
@@ -197,6 +359,7 @@ int main() {
   // Window1 * Window2 = new Window1();
 
   // Window2->windowEx();
+
 
 
   shared_ptr<MainWindow> window(new MainWindow());
