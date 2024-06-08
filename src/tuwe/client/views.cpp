19fe3c35162db997e1b23f66ea81b653c428d0b2
Zamurovka <victor17razumov@gmail.com>
2024-06-08 16:03:48 +0300

    Killed all compinents

--- src/tuwe/client/views.cpp
+++ src/tuwe/client/views.cpp
@@ -18,28 +18,60 @@ MainView::MainView(const Application &app) : app(app) {
 }
 
 ftxui::Element MainView::Render() {
-  //int humidity = 50;
-  //std::string humidityString = std::to_string(humidity) + "%";
-  //ftxui::Component humiditySlider = ftxui::Slider("", &humidity, 0, 100, 1);
-  auto ContainerBlocks = ftxui::Container::Horizontal({
+  // int humidity = 50;
+  // std::string humidityString = std::to_string(humidity) + "%";
+  // ftxui::Component humiditySlider = ftxui::Slider("", &humidity, 0, 100, 1);
+ 
 
-  });
 
-  auto container_general_conditions = ftxui::Container::Horizontal({
-
-  });
-
-  auto blocks = ftxui::Renderer(ContainerBlocks, [&] {
-    return ftxui::vbox({
-        ftxui::hbox({
-
-        }),
-
-    });
-  });
-
-  auto block_hourly_forecast = ftxui::Renderer(ContainerBlocks, [&] {
-    return ftxui::vbox({
+  
+  return ftxui::vbox({
+             ftxui::hbox({
+                 ftxui::color(ftxui::Color::RosyBrown, ftxui::text("TUWE — ")),
+                 ftxui::color(ftxui::Color::SkyBlue2, ftxui::text("Terminal User Weather Explorer")),
+             }) |
+                 ftxui::borderEmpty,
+             ftxui::hbox({
+                 ftxui::separatorEmpty(),
+                 ftxui::text("🚩"),
+                 ftxui::separatorEmpty(),
+                 this->locationInput->Render(),
+                 ftxui::separatorEmpty(),
+             }) | ftxui::borderRounded,
+             ftxui::vbox({
+                 ftxui::text("Now"),
+                 ftxui::hbox({
+                     ftxui::vbox({
+                         ftxui::hbox({
+                             ftxui::text("18℃"),
+                             ftxui::separatorEmpty(),
+                             ftxui::separatorEmpty(),
+                             ftxui::text("🌤"),
+                         }) | ftxui::borderEmpty,
+                         ftxui::hbox({
+                             ftxui::hbox({
+                                 ftxui::text("High:"),
+                                 ftxui::separatorEmpty(),
+                                 ftxui::text("19℃"),
+                                 ftxui::separatorEmpty(),
+                             }),
+                             ftxui::separatorEmpty(),
+                             ftxui::hbox({
+                                 ftxui::separatorEmpty(),
+                                 ftxui::text("Low:"),
+                                 ftxui::separatorEmpty(),
+                                 ftxui::text("4℃"),
+                             }),
+                         }),
+                     }),
+                     ftxui::filler() | ftxui::flex,
+                     ftxui::vbox({
+                         ftxui::text("Mostly sunny"),
+                         ftxui::text("Feels like 18℃"),
+                     }) | ftxui::borderEmpty,
+                 }),
+             }) | ftxui::borderEmpty,
+             ftxui::vbox({
         ftxui::text(" Часовая сводка"),
         ftxui::hbox({
             ftxui::filler(),
@@ -77,12 +109,8 @@ ftxui::Element MainView::Render() {
             ftxui::filler(),
         }) | ftxui::borderRounded,
 
-    });
-  });
-  auto block_general_conditions = ftxui::Renderer(ContainerBlocks, [&] {
-    return ftxui::vbox({
-
-        // первые два
+    }),
+             ftxui::vbox({
 
         ftxui::hbox({
             ftxui::hbox({
@@ -179,56 +207,7 @@ ftxui::Element MainView::Render() {
 
         }) | ftxui::flex,
 
-    });
-  });
-  return ftxui::vbox({
-             ftxui::hbox({
-                 ftxui::color(ftxui::Color::RosyBrown, ftxui::text("TUWE — ")),
-                 ftxui::color(ftxui::Color::SkyBlue2, ftxui::text("Terminal User Weather Explorer")),
-             }) |
-                 ftxui::borderEmpty,
-             ftxui::hbox({
-                 ftxui::separatorEmpty(),
-                 ftxui::text("🚩"),
-                 ftxui::separatorEmpty(),
-                 this->locationInput->Render(),
-                 ftxui::separatorEmpty(),
-             }) | ftxui::borderRounded,
-             ftxui::vbox({
-                 ftxui::text("Now"),
-                 ftxui::hbox({
-                     ftxui::vbox({
-                         ftxui::hbox({
-                             ftxui::text("18℃"),
-                             ftxui::separatorEmpty(),
-                             ftxui::separatorEmpty(),
-                             ftxui::text("🌤"),
-                         }) | ftxui::borderEmpty,
-                         ftxui::hbox({
-                             ftxui::hbox({
-                                 ftxui::text("High:"),
-                                 ftxui::separatorEmpty(),
-                                 ftxui::text("19℃"),
-                                 ftxui::separatorEmpty(),
-                             }),
-                             ftxui::separatorEmpty(),
-                             ftxui::hbox({
-                                 ftxui::separatorEmpty(),
-                                 ftxui::text("Low:"),
-                                 ftxui::separatorEmpty(),
-                                 ftxui::text("4℃"),
-                             }),
-                         }),
-                     }),
-                     ftxui::filler() | ftxui::flex,
-                     ftxui::vbox({
-                         ftxui::text("Mostly sunny"),
-                         ftxui::text("Feels like 18℃"),
-                     }) | ftxui::borderEmpty,
-                 }),
-             }) | ftxui::borderEmpty,
-             block_hourly_forecast->Render(),
-             block_general_conditions->Render(),
+    }),
              ftxui::hbox({
                  ftxui::text("❄ ") | ftxui::borderEmpty,
                  ftxui::separatorEmpty(),
