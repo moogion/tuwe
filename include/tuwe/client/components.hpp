d4297c749c19ea1b234fd998b3c07f367ecd1632
Yotu Willo <28297052+moogion@users.noreply.github.com>
2024-06-29 19:24:11 +0300

    feet: normalize & add component make function; style: migrate to fully use Google Style convention;

--- include/tuwe/client/components.hpp
+++ include/tuwe/client/components.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <ftxui/component/component_base.hpp>
+#include <memory>
 
 namespace tuwe::client {
 
@@ -11,26 +12,29 @@ enum ScrollerDirection {
 };
 
 class Scroller : public ftxui::ComponentBase {
+ public:
+  explicit Scroller(ftxui::Component child) : Scroller(child, ScrollerDirection::ALL) {};
+  explicit Scroller(ftxui::Component child, const ScrollerDirection direction);
+
  private:
   ftxui::Element Render() override;
 
-  ScrollerDirection direction;
+  ScrollerDirection direction_;
 
-  int scrollWidth;
-  int scrollHeight;
+  int scroll_width_;
+  int scroll_height_;
 
-  int sizeWidth;
-  int sizeHeight;
+  int size_width_;
+  int size_height_;
 
-  ftxui::Box box;
+  ftxui::Box box_;
 
   bool OnEvent(ftxui::Event event) override;
 
   bool Focusable() const override;
-
- public:
-  explicit Scroller(ftxui::Component child) : Scroller(child, ScrollerDirection::ALL){};
-  explicit Scroller(ftxui::Component child, const ScrollerDirection direction);
 };
+
+using ScrollerComponent = std::shared_ptr<Scroller>;
+ScrollerComponent MakeScroller(ftxui::Component child, const ScrollerDirection direction = ScrollerDirection::ALL);
 
 }  // namespace tuwe::client
--- src/tuwe/client/components.cpp
+++ src/tuwe/client/components.cpp
@@ -1,34 +1,38 @@
 #include "tuwe/client/components.hpp"
 
 #include <ftxui/component/event.hpp>
-#include <ftxui/dom/node.hpp>
-#include <iostream>
+#include <ftxui/component/mouse.hpp>
+#include <memory>
 
 namespace tuwe::client {
 
+ScrollerComponent MakeScroller(ftxui::Component child, const ScrollerDirection direction) {
+  return std::make_shared<Scroller>(std::move(child), direction);
+}
+
 Scroller::Scroller(ftxui::Component child, const ScrollerDirection direction)
-    : scrollWidth(0), scrollHeight(0) {
+    : scroll_width_(0), scroll_height_(0) {
   this->Add(child);
-  this->direction = std::move(direction);
+  this->direction_ = std::move(direction);
 }
 
 ftxui::Element Scroller::Render() {
   ftxui::Element background = ftxui::ComponentBase::Render();
   background->ComputeRequirement();
 
-  const int focusOffsetWidth = this->box.x_max - this->box.x_min - 1;
-  const int focusOffsetHeight = this->box.y_max - this->box.y_min - 1;
+  const int focusOffsetWidth = this->box_.x_max - this->box_.x_min - 1;
+  const int focusOffsetHeight = this->box_.y_max - this->box_.y_min - 1;
 
   const ftxui::Requirement requirement = background->requirement();
-  this->sizeWidth = requirement.min_x - focusOffsetWidth;
-  this->sizeHeight = requirement.min_y - focusOffsetHeight;
+  this->size_width_ = requirement.min_x - focusOffsetWidth;
+  this->size_height_ = requirement.min_y - focusOffsetHeight;
 
   background |= ftxui::focusPosition(
-      focusOffsetWidth / 2 + this->scrollWidth,
-      focusOffsetHeight / 2 + this->scrollHeight
+      focusOffsetWidth / 2 + this->scroll_width_,
+      focusOffsetHeight / 2 + this->scroll_height_
   );
 
-  switch (this->direction) {
+  switch (this->direction_) {
     case ScrollerDirection::ALL: {
       background |= ftxui::vscroll_indicator;
       background |= ftxui::frame;
@@ -52,7 +56,7 @@ ftxui::Element Scroller::Render() {
     background |= ftxui::focus;
   }
 
-  return std::move(background) | ftxui::reflect(this->box);
+  return std::move(background) | ftxui::reflect(this->box_);
 }
 
 bool Scroller::OnEvent(ftxui::Event event) {
@@ -62,65 +66,65 @@ bool Scroller::OnEvent(ftxui::Event event) {
 
   ftxui::Mouse& mouse = event.mouse();
 
-  if (event.is_mouse() && !box.Contain(mouse.x, mouse.y)) {
+  if (event.is_mouse() && !box_.Contain(mouse.x, mouse.y)) {
     return false;
   }
 
-  int scrolledWidth = this->scrollWidth;
-  int scrolledHeight = this->scrollHeight;
+  int scrolledWidth = this->scroll_width_;
+  int scrolledHeight = this->scroll_height_;
 
   if (event == ftxui::Event::ArrowUp || event == ftxui::Event::Character('k') ||
       (event.is_mouse() && mouse.button == ftxui::Mouse::WheelUp)) {
-    if (this->direction == ScrollerDirection::WIDTH) {
-      this->scrollWidth--;
+    if (this->direction_ == ScrollerDirection::WIDTH) {
+      this->scroll_width_--;
     }
-    this->scrollHeight--;
+    this->scroll_height_--;
   }
 
   if (event == ftxui::Event::ArrowDown || event == ftxui::Event::Character('j') ||
       (event.is_mouse() && mouse.button == ftxui::Mouse::WheelDown)) {
-    if (this->direction == ScrollerDirection::WIDTH) {
-      this->scrollWidth++;
+    if (this->direction_ == ScrollerDirection::WIDTH) {
+      this->scroll_width_++;
     }
-    this->scrollHeight++;
+    this->scroll_height_++;
   }
 
   if (event == ftxui::Event::ArrowLeft || event == ftxui::Event::Character('h')) {
-    this->scrollWidth--;
+    this->scroll_width_--;
   }
 
   if (event == ftxui::Event::ArrowRight || event == ftxui::Event::Character('l')) {
-    this->scrollWidth++;
+    this->scroll_width_++;
   }
 
   if (event == ftxui::Event::PageUp) {
-    if (this->direction == ScrollerDirection::WIDTH) {
-      this->scrollWidth -= this->box.x_max - box.x_min;
+    if (this->direction_ == ScrollerDirection::WIDTH) {
+      this->scroll_width_ -= this->box_.x_max - box_.x_min;
     }
-    this->scrollHeight -= this->box.y_max - box.y_min;
+    this->scroll_height_ -= this->box_.y_max - box_.y_min;
   }
 
   if (event == ftxui::Event::PageDown) {
-    if (this->direction == ScrollerDirection::WIDTH) {
-      this->scrollWidth += this->box.x_max - box.x_min;
+    if (this->direction_ == ScrollerDirection::WIDTH) {
+      this->scroll_width_ += this->box_.x_max - box_.x_min;
     }
-    this->scrollHeight += this->box.y_max - box.y_min;
+    this->scroll_height_ += this->box_.y_max - box_.y_min;
   }
 
   if (event == ftxui::Event::Home) {
-    this->scrollWidth = 0;
-    this->scrollHeight = 0;
+    this->scroll_width_ = 0;
+    this->scroll_height_ = 0;
   }
 
   if (event == ftxui::Event::End) {
-    this->scrollWidth = this->sizeWidth;
-    this->scrollHeight = this->sizeHeight;
+    this->scroll_width_ = this->size_width_;
+    this->scroll_height_ = this->size_height_;
   }
 
-  this->scrollWidth = std::max(0, std::min(this->sizeWidth - 1, this->scrollWidth));
-  this->scrollHeight = std::max(0, std::min(this->sizeHeight - 1, this->scrollHeight));
+  this->scroll_width_ = std::max(0, std::min(this->size_width_ - 1, this->scroll_width_));
+  this->scroll_height_ = std::max(0, std::min(this->size_height_ - 1, this->scroll_height_));
 
-  return scrolledWidth != this->scrollWidth || scrolledHeight != this->scrollHeight;
+  return scrolledWidth != this->scroll_width_ || scrolledHeight != this->scroll_height_;
 }
 
 bool Scroller::Focusable() const {
